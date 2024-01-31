011abb5483a05c238c3b9eab7afe9192fb70d495
NIRMALRAJ1506 <nirmalraj75500@gmail.com>
2024-01-31 22:41:28 +0530

    commit

---
+++ InsuranceManagementSystem/InsuranceManagementSystem/UI/Models/PolicyViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using DAL;
+
+namespace UI.Models
+{
+    public class PolicyViewModel
+    {
+        [Required(ErrorMessage = "Policy Number is required")]
+        public string PolicyNumber { get; set; }
+
+
+
+        [Required(ErrorMessage = "Applied Date is required")]
+        [DataType(DataType.Date)]
+        public DateTime AppliedDate { get; set; }
+
+        // Example: Policy Type (Life, Health, Auto, etc.)
+        public string Category { get; set; }
+        public PolicyType PolicyType { get; set; }
+
+        public double Price { get; set; }
+
+        // Add other properties as needed
+    }
+
+
+}
